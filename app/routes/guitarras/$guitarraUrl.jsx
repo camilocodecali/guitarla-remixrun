1e96436f6b285d9316a2e68e9ec43ba8dfd3f407
CamiloR <camilorodriguezco7@gmail.com>
2022-11-29 23:09:07 -0500

    se agrego el carrito en el menú, se creo pagina de carrito, se configuro boton de agregar carrito para que cree un arreglo del producto agregado y se valido si hay alguna opcion de cantidad seleccionada

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useLoaderData } from '@remix-run/react'
 import {getGuitarra} from '~/models/guitarras.server'
 
@@ -33,9 +34,27 @@ export function meta({data}){
 
 function Guitarra() {
 
+  const [ cantidad, setCantidad ] = useState(0)
   const guitarra = useLoaderData()
   const {nombre, descripcion, imagen, precio} = guitarra.data[0].attributes
 
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if(cantidad <1){
+      alert('Debes seleccionar una cantidad')
+      return
+    }
+
+    const guitarraSeleccionada = {
+      id: guitarra.data[0].id,
+      imagen: imagen.data.attributes.url,
+      nombre,
+      precio,
+      cantidad
+    }
+    console.log(guitarraSeleccionada);
+  }
 
   return (
     <div className='guitarra'>
@@ -44,6 +63,24 @@ function Guitarra() {
         <h3>{nombre}</h3>
         <p className='texto'>{descripcion}</p>
         <p className='precio'>${precio}</p>
+
+        <form onSubmit={handleSubmit} className='formulario'>
+          <label htmlFor="cantidad">Cantidad</label>
+          <select name="" 
+            onChange={e=> setCantidad(+e.target.value)}
+            id="cantidad">
+            <option value="0">-- Seleccione --</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <input 
+            type="submit"
+            value="Agregar al carrito"
+          />
+        </form>
       </div>
     </div>
   )
